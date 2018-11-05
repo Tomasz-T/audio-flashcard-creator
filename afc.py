756eb0deb8153032a7a04c2347da89ade7ae950c
Tomasz-T <tom_t@o2.pl>
2018-11-05 22:11:14 +0100

    output added

--- afc.py
+++ afc.py
@@ -4,70 +4,45 @@ Created on Thu Nov  1 06:08:26 2018
 
 @author: Tomasz Truszkowski
 """
+import os
+from pathlib import Path
 
+# choose path
+main_directory = Path('C:/Users/(...)/audio-flashcard-creator/')
 
 # %%
 #get the list to translate
-input_text = open('.\input\input_text.txt','r')
+input_text = open(main_directory / 'input.txt','r')
 input_list = input_text.read().split('\n')
 input_text.close
 
-
 # %%
-#for tests
-#input_list = input_list[:5]
-#print(input_list)
-
-
-# %%
-# translate
+# translate  might be unstable
 from googletrans import Translator
-# might be unstable
 
 translator = Translator()
 translated = translator.translate(input_list,src= 'en', dest = 'pl')
 
-
-# %%
-#write to translated list
-translated_list=[]
-
-for translation in translated:
-    print(translation.origin, ' / ', translation.text)
-    translated_list.append((translation.origin, translation.text))
-        
-
-# %%
-# save to file in case verification needed
-    
-with open('.\output\output_text.txt','w') as translated_file:
-    translated_file.write(str(translated_list))
-    
-    
   # %%
+# files for silence
+wait_4_sec =  open(main_directory / "media/Silence04s.mp3",'rb').read()
+wait_1_sec =  open(main_directory / "media/Silence01s.mp3",'rb').read()
+
+# %%
 # translation gTTS
 from gtts import gTTS
+import time
 
-for tts_input in translated_list: 
-    print(tts_input[0])
-    print(tts_input[1])
-    tts_from = gTTS(tts_input[0], lang='en')
-    tts_from.save('.\output\\' + tts_input[0] +'.mp3')
-    tts_to = gTTS(tts_input[1], lang='pl')
-    tts_to.save('.\output\\' + tts_input[0] +'_pl.mp3')
+with open(main_directory /'output_mp3/wynik_zlozenie4.mp3', 'wb') as out_file:
+    for translation in translated: 
+        tts_from = gTTS(translation.origin, lang='en')
+        time.sleep(2) #don't mess with Google ;)
+        tts_to = gTTS(translation.text, lang='pl')
+        #write to file
+        tts_from.write_to_fp(out_file)
+        out_file.write(wait_4_sec)
+        tts_to.write_to_fp(out_file)
+        out_file.write(wait_1_sec)
+print('finished')
 
-    
-    
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+ 
